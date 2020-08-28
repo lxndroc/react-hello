1d6c3600f92de91672ccc9cbf31ff2b47128ed7e
+Alexandros <42879364+lxndroc@users.noreply.github.com>
2020-08-28 15:22:42 +0100

    Updates

---
+++ precache-manifest.b8ae2289b3b93bec9dde4e0a271cb872.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "594dcddf66a25b92dc9dfea03d4049f7",
+    "url": "/react-hello/index.html"
+  },
+  {
+    "revision": "4fc7e2f409a255692f0c",
+    "url": "/react-hello/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "eb6ffb3945c9bd6953b3",
+    "url": "/react-hello/static/js/2.72442180.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hello/static/js/2.72442180.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4fc7e2f409a255692f0c",
+    "url": "/react-hello/static/js/main.40412308.chunk.js"
+  },
+  {
+    "revision": "fa5e7779ab5f27fabd84",
+    "url": "/react-hello/static/js/runtime-main.4e8078e1.js"
+  }
+]);
