58334853ba5ce6754bb22761df5055be5d720bd0
+Alexandros <lxndroc@gmail.com>
2020-08-28 14:34:27 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://lirp-cdn.multiscreensite.com/5b421b6a/dms3rep/multi/opt/cmbLgOutl-1920w.png" className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
